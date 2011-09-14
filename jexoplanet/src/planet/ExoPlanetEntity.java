255dfe25d15fe1a1a2a2c19f9d8fa29df331290b
David Greenfield <dgreenfield0@gmail.com>
2011-09-14 00:22:45 -0400

    Added planet package, and moved exoplanet pojo under it. Updated Readme with more info about project.

---
+++ jexoplanet/src/planet/ExoPlanetEntity.java
@@ -0,0 +1,14 @@
+package planet;
+
+/**
+ * 
+ * @author David Greenfield
+ * This Class represents the ExoPlanet data that is pulled from the NsTed database.  
+ * Overview of the NStED exoPlanet Table columns can be found here.  
+ * http://nsted.ipac.caltech.edu/NStED/docs/API_planet_columns.html
+ */
+
+
+public class ExoPlanetEntity {
+
+}
